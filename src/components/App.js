a89d84253fead1f50ab6e8e4e3143743d27fab79
Muhammad Yana Mulyana <muhammadyanaa@gmail.com>
2019-07-23 20:01:50 +0700

    :art: add onVideoSelect method to handle selected video / show video

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,36 @@
-import React from 'react';
-import SearchBar from './SearchBar';
-import youtube from '../apis/youtube';
-import VideoList from './VideoList';
+import React from "react";
+import SearchBar from "./SearchBar";
+import youtube from "../apis/youtube";
+import VideoList from "./VideoList";
 
 class App extends React.Component {
-  state = { videos: [] };
+  state = { videos: [], selectedVideo: null };
   // add async and await for callback return from axios
   onTermSubmit = async term => {
     // console.log(term);
-    const response = await youtube.get('/search',{
+    const response = await youtube.get("/search", {
       params: {
         q: term
       }
     });
     // console.log(response.data.items);
-    this.setState({ videos: response.data.items })
+    this.setState({ videos: response.data.items });
   };
 
-  render(){
-    return(
-      <div className='ui container'>
+  onVideoSelect = video => {
+    console.log("from video select", video);
+  };
+
+  render() {
+    return (
+      <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoList videos={this.state.videos}/>
-      </div> 
-    ) 
+        <VideoList
+          onVideoSelect={this.onVideoSelect}
+          videos={this.state.videos}
+        />
+      </div>
+    );
   }
 }
 
