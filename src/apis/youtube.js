f9906d9985255749257a94dbbceaca03cd1ec9b6
Muhammad Yana Mulyana <muhammadyanaa@gmail.com>
2019-07-23 20:54:05 +0700

    :man_technologist: add video detail component

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,14 +1,13 @@
-import axios from 'axios';
+import axios from "axios";
 
 // const KEY = 'AIzaSyA5RB6Wh_YU98tUDSK2LC9tNFcdn4l1Vjw';
 const KEY = "AIzaSyBarBDkRUuqUSBoZcvbZrVfxfFKYBOqdxs";
 
 export default axios.create({
-  baseURL: 'https://www.googleapis.com/youtube/v3',
+  baseURL: "https://www.googleapis.com/youtube/v3",
   params: {
-    part: 'snippet',
-    maxResults: 5,
+    part: "snippet",
+    maxResults: 8,
     key: KEY
   }
 });
-
