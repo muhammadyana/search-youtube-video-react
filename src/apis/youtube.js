400e152f52957930bf1ef069bc2ff582610d96d7
Muhammad Yana Mulyana <muhammadyanaa@gmail.com>
2019-07-23 13:01:24 +0700

    :man_technologist: initial commit :tada:

---
+++ src/apis/youtube.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+// const KEY = 'AIzaSyA5RB6Wh_YU98tUDSK2LC9tNFcdn4l1Vjw';
+const KEY = "AIzaSyBarBDkRUuqUSBoZcvbZrVfxfFKYBOqdxs";
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    maxResults: 5,
+    key: KEY
+  }
+});
+
---
+++ src/components/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+import youtube from '../apis/youtube';
+
+class App extends React.Component {
+  state = { videos: [] };
+  onTermSubmit = async term => {
+    // console.log(term);
+    const response = await youtube.get('/search',{
+      params: {
+        q: term
+      }
+    });
+    console.log(response.data.items);
+    this.setState({ videos: response.data.items })
+  };
+
+  render(){
+    return(
+      <div className='ui container'>
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+        i have {this.state.videos.length} videos.
+      </div> 
+    ) 
+  }
+}
+
+export default App;
